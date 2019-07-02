2fbe6d005df3e936632d7301fc25313991819837
johngao2 <john.gao@queensu.ca>
2019-07-02 18:52:43 -0400

    sheets

--- laddermasterbot.py
+++ laddermasterbot.py
@@ -1010,51 +1010,51 @@ async def changeLadderName(ctx, oldName, newName):
 
 # SPREADSHEET PART OF THE CODE ---------------------------------------------------------------------------------------
 def updateSheet(ladders):
-    # ladders = loadLadders()
+    ladders = loadLadders()
 
-    # # use creds to create a client to interact with the Google Drive API
-    # scope = [
-    #     "https://spreadsheets.google.com/feeds",
-    #     "https://www.googleapis.com/auth/drive",
-    # ]
-    # creds = ServiceAccountCredentials.from_json_keyfile_name(
-    #     "client_secret.json", scope
-    # )
-    # client = gspread.authorize(creds)
+    # use creds to create a client to interact with the Google Drive API
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "client_secret.json", scope
+    )
+    client = gspread.authorize(creds)
 
-    # sheet = client.open("QFGC Linear Ladder Sheets")
+    sheet = client.open("QFGC Linear Ladder Sheets")
 
-    # # iterate over ladder dict
-    # for ladderName, ladderData in ladders.items():
-    #     ladderSheet = sheet.worksheet(ladderName)
+    # iterate over ladder dict
+    for ladderName, ladderData in ladders.items():
+        ladderSheet = sheet.worksheet(ladderName)
 
-    #     # clear cells F13:I33
-    #     cell_list = ladderSheet.range('F13:I33')
+        # clear cells F13:I33
+        cell_list = ladderSheet.range('F13:I33')
 
-    #     xx = 0
-    #     yy = 0
+        xx = 0
+        yy = 0
 
-    #     for cell in cell_list:
-    #         cell.value = ''
+        for cell in cell_list:
+            cell.value = ''
 
-    #         yy = cell.row-13
-    #         xx = cell.col-6
-    #         if yy < len(ladderData):
-    #             if xx == 0:
-    #                 cell.value = yy+1
-    #             elif xx == 1:
-    #                 cell.value = ladderData[yy].tag
-    #             elif xx == 2:
-    #                 cList = ""
-    #                 for c in ladderData[yy].characters:
-    #                     cList += c + ", "
+            yy = cell.row-13
+            xx = cell.col-6
+            if yy < len(ladderData):
+                if xx == 0:
+                    cell.value = yy+1
+                elif xx == 1:
+                    cell.value = ladderData[yy].tag
+                elif xx == 2:
+                    cList = ""
+                    for c in ladderData[yy].characters:
+                        cList += c + ", "
 
-    #                 cList = cList[:-2]
-    #                 cell.value = cList
-    #             elif xx == 3:
-    #                 cell.value = ladderData[yy].discordid
+                    cList = cList[:-2]
+                    cell.value = cList
+                elif xx == 3:
+                    cell.value = ladderData[yy].discordid
 
-    #     ladderSheet.update_cells(cell_list)
+        ladderSheet.update_cells(cell_list)
     return
 
 
