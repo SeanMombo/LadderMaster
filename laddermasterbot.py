d432a479bd734ddbde79461a67194fbd2dbce370
John Gao <john.gao@queensu.ca>
2019-07-04 19:15:25 -0400

    Update laddermasterbot.py

--- laddermasterbot.py
+++ laddermasterbot.py
@@ -824,7 +824,9 @@ async def confirm(ctx, winner: discord.Member, score, ladderName):
 
         # update cumulative totals
         winner.gameWins += int(winScore)
+        winner.gameLosses += int(lossScore)
         loser.gameLosses += int(winScore)
+        loser.gameWins += int(lossScore)
         winner.setWins += 1
         loser.setLosses += 1
 
