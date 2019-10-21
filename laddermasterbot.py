77d6fa30ad3efabaf6be2e38a02f20675d947bf6
johngao2 <john.gao@queensu.ca>
2019-10-21 14:29:31 -0400

    assimilated tekken bot

--- laddermasterbot.py
+++ laddermasterbot.py
@@ -7,11 +7,16 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 import sched
+import os, sys
+import datetime
+import asyncio
 import time
 import pickle
 import operator
 import traceback
 from datetime import date, timedelta, datetime
+
+import tkfinder    # for tekken bot
 
 # global vars
 version_num = "1.4"
@@ -35,6 +40,63 @@ inktober_cues = ['Ring', 'Mindless', 'Bait', 'Freeze', 'Build', 'Husky', 'Enchan
                  'Legend', 'Wild', 'Ornament', 'Misfit', 'Sling', 'Tread', 'Treasure',
                  'Ghost', 'Ancient', 'Dizzy', 'Tasty', 'Dark', 'Coat', 'Ride',
                  'Injured', 'Catch', 'Ripe']
+
+# Dict for searching special move types
+move_types = {  'ra': 'Rage art',
+                'rage_art': 'Rage art',
+                'rd': 'Rage drive',
+                'rage_drive': 'Rage drive',
+                'wb': 'Wall bounce',
+                'wall_bounce': 'Wall bounce',
+                'ts': 'Tail spin',
+                'tail_spin': 'Tail spin',
+                'screw': 'Tail spin',
+                'homing': 'Homing',
+                'homari': 'Homing',
+                'armor': 'Power crush',
+                'armori': 'Power crush',
+                'pc': 'Power crush',
+                'power': 'Power crush',
+                'power_crush': 'Power crush'}
+
+# TEKKEN STUFF ########################################
+def move_embed(character, move):
+    '''Returns the embed message for character and move'''
+    embed = discord.Embed(title=character['proper_name'], 
+            colour=0x00EAFF,
+            url=character['online_webpage'],
+            description='Move: ' + move['Command'])
+    
+    embed.set_thumbnail(url=character['portrait'])
+    embed.add_field(name='Property', value=move['Hit level'])
+    embed.add_field(name='Damage', value=move['Damage'])
+    embed.add_field(name='Startup', value='i' + move['Start up frame'])
+    embed.add_field(name='Block', value=move['Block frame'])
+    embed.add_field(name='Hit', value=move['Hit frame'])
+    embed.add_field(name='Counter Hit', value=move['Counter hit frame'])
+    embed.add_field(name='Notes', value=move['Notes'])
+    
+    return embed
+
+def move_list_embed(character, move_list, move_type):
+    '''Returns the embed message for a list of moves matching to a special move type'''
+    desc_string = ''
+    for move in move_list:
+        desc_string += move + '\n'
+
+    embed = discord.Embed(title=character['proper_name'] + ' ' + move_type.lower() + ':',
+            colour=0x00EAFF,
+            description=desc_string)
+
+    return embed
+
+def error_embed(err):
+    embed = discord.Embed(title='Error',
+            colour=0xFF4500,
+            description=err)
+
+    return embed
+### TEKKEN STUFF #################################
 
 # try:
 
@@ -203,10 +265,10 @@ async def on_command_error(ctx, error):
 
     if isinstance(error, commands.MissingRole):
         await ctx.send("You don't have permission to use this command YOU IDIOT.")
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send(
-            "That command doesn't exist, type !help or !helpadmin for a list of commands"
-        )
+    # if isinstance(error, commands.CommandNotFound):   # temporarily muted to help with tekken
+    #     await ctx.send(
+    #         "That command doesn't exist, type !help or !helpadmin for a list of commands"
+    #     )
     if isinstance(error, commands.MissingRequiredArgument):
         if str(ctx.command) == "ladder":
             await ctx.send("The correct usage is !ladder <game> or !l <game>")
@@ -245,6 +307,107 @@ async def on_command_error(ctx, error):
         if str(ctx.command) == "changeLadderName":
             await ctx.send("The correct usage is !changeLadderName <oldname> <newname>")
 
+### TEKKEN STUFF ###########################################
+@bot.event
+async def on_message(message):
+    '''This has the main functionality of the bot. It has a lot of
+    things that would be better suited elsewhere but I don't know
+    if I'm going to change it.
+    '''
+    channel = message.channel
+    if message.content.startswith('!') and (channel.name == '3dfighters' or channel.name == 'test-chamber-2'):
+
+        user_message = message.content
+        user_message = user_message.replace('!', '')
+        user_message_list = user_message.split(' ', 1)
+
+        if len(user_message_list) <= 1:
+            # malformed command
+            return
+
+        chara_name = user_message_list[0].lower()
+        chara_move = user_message_list[1]
+        if chara_name == 'armor' or chara_name == 'ak':
+            chara_name = 'armor_king'
+        elif chara_name == 'dj' or chara_name == 'dvj' or chara_name == 'djin' or chara_name == 'devil' or chara_name == 'deviljin' or chara_name == 'diablojim' or chara_name == 'taika-jim':
+            chara_name = 'devil_jin'
+        elif chara_name == 'sergei' or chara_name == 'drag' or chara_name == 'dragu':
+            chara_name = 'dragunov'
+        elif chara_name == 'goose':
+            chara_name = 'geese'
+        elif chara_name == 'hwo' or chara_name == 'hwoa':
+            chara_name = 'hwoarang'
+        elif chara_name == 'jack':
+            chara_name = 'jack7'
+        elif chara_name == 'julle':
+            chara_name = 'julia'
+        elif chara_name == 'chloe' or chara_name == 'lc' or chara_name == 'lucky':
+            chara_name = 'lucky_chloe'
+        elif chara_name == 'hei' or chara_name == 'hessu' or chara_name == 'heiska':
+            chara_name = 'heihachi'
+        elif chara_name == 'kata' or chara_name == 'kat':
+            chara_name = 'katarina'
+        elif chara_name == 'kaz' or chara_name == 'kazze':
+            chara_name = 'kazuya'
+        elif chara_name == 'karhu' or chara_name == 'panda':
+            chara_name = 'kuma'
+        elif chara_name == 'mara':
+            chara_name = 'marduk'
+        elif chara_name == 'master' or chara_name == 'raven' or chara_name == 'mraven' or chara_name == 'masterraven':
+            chara_name = 'master_raven'
+        elif chara_name == 'nocto':
+            chara_name = 'noctis'
+        elif chara_name == 'pave':
+            chara_name = 'paul'
+        elif chara_name == 'sha':
+            chara_name = 'shaheen'
+        elif chara_name == 'yoshi':
+            chara_name = 'yoshimitsu'
+        elif chara_name == 'ling':
+           chara_name = 'xiaoyu'
+
+        character = tkfinder.get_character(chara_name)
+        if character is not None:
+            if chara_move.lower() in move_types:
+                chara_move = chara_move.lower()
+                move_list = tkfinder.get_by_move_type(character, move_types[chara_move])
+                if  len(move_list) < 1:
+                    embed = error_embed('No ' + move_types[chara_move].lower() + ' for ' + character['proper_name'])
+                    msg = await channel.send(embed=embed, delete_after=150)
+                elif len(move_list) == 1:
+                    move = tkfinder.get_move(character, move_list[0], False)
+                    embed = move_embed(character, move)
+                    msg = await channel.send(embed=embed, delete_after=300)
+                elif len(move_list) > 1:
+                    embed = move_list_embed(character, move_list, move_types[chara_move])
+                    msg = await channel.send(embed=embed, delete_after=300)
+
+            else:
+                move = tkfinder.get_move(character, chara_move, True)
+            
+                #First checks the move as case sensitive, if it doesn't find it
+                #it checks it case unsensitive
+            
+                if move is not None:
+                    embed = move_embed(character, move) 
+                    msg = await channel.send(embed=embed, delete_after=300)
+                else:
+                    move = tkfinder.get_move(character, chara_move, False)
+                    if move is not None:
+                        embed = move_embed(character, move)
+                    
+                        msg = await channel.send(embed=embed, delete_after=300)
+                    else:
+                        embed = error_embed('Move not found: ' + chara_move)
+                        msg = await channel.send(embed=embed, delete_after=150)
+        else:
+            bot_msg = 'Character ' + chara_name + ' does not exist.'
+            embed = error_embed(bot_msg)
+            msg = await message.channel.send(embed=embed, delete_after=150)
+
+            return
+    await bot.process_commands(message)
+### TEKKEN STUFF ##############################
 
 # version
 @bot.command()
